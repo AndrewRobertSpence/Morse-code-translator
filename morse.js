baf1055d3859bf7460d80cc53aba87d8ebd353e4
Andrew Spence <aspence3595@gmail.com>
2024-04-10 16:29:42 +1200

    added function for inputing morse code or text

--- morse.js
+++ morse.js
@@ -1,9 +1,26 @@
+document.addEventListener('DOMContentLoaded', function() {
 // identify all DOM elements
-const form = document.querySelector('#morse__form');
-const textInput = document.querySelector('#morse__textInput');
-const translateButton = document.querySelector('#morse__translateButton');
-const resetButton = document.querySelector('#morse__resetButton');
-const output = document.querySelector('#morse__output');
+const textInput = document.querySelector("#morse__textInput");
+const translateButton = document.querySelector("#morse__translateButton");
+const resetButton = document.querySelector("#morse__resetButton");
+const output = document.querySelector("#morse__output");
+
+// add EventListener to Function input morse code or text
+translateButton.addEventListener("click", inputMorseCodeOrText);
+function inputMorseCodeOrText(event) {
+  // prevent form from submitting
+  event.preventDefault();
+  // get the value of the text input
+  const inputText = textInput.value;
+  // check if the input is morse code or text
+  if (inputText.match(/^[\.|\-]+$/)) {
+    // morse code
+    output.textContent = translateMorseCode(inputText);
+  } else {
+    // text
+    output.textContent = translateText(inputText);
+  }
+}
 
 
-
+});
