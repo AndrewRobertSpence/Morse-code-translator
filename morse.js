85f999b3634ece205bbf323522f42454ac2f1559
Andrew Spence <aspence3595@gmail.com>
2024-04-10 15:21:32 +1200

    added morse code alphabet object and added DOM elements

---
+++ morse.js
@@ -0,0 +1,9 @@
+// identify all DOM elements
+const form = document.querySelector('#morse__form');
+const textInput = document.querySelector('#morse__textInput');
+const translateButton = document.querySelector('#morse__translateButton');
+const resetButton = document.querySelector('#morse__resetButton');
+const output = document.querySelector('#morse__output');
+
+
+
