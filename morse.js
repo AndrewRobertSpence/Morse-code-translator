6f4f15fcfcac515b7ba81803db1ffc83f22fe830
Andrew Spence <aspence3595@gmail.com>
2024-04-11 13:09:19 +1200

    added function for translate from morse code to english.  But need to debug

--- morse.js
+++ morse.js
@@ -5,11 +5,7 @@ const translateButton = document.querySelector("#morse__translateButton");
 const resetButton = document.querySelector("#morse__resetButton");
 const output = document.querySelector("#morse__output");
 
-// add EventListener to Function input morse code or text
-translateButton.addEventListener("click", inputMorseCodeOrText);
-function inputMorseCodeOrText(event) {
-  // prevent form from submitting
-  event.preventDefault();
+const inputMorseCodeOrText = () => {
   // get the value of the text input
   const inputText = textInput.value;
   // check if the input is morse code or text
@@ -22,15 +18,17 @@ function inputMorseCodeOrText(event) {
   }
 }
 
-// Function translate text to morse code
+// add EventListener to Function input morse code or text
+translateButton.addEventListener("click", inputMorseCodeOrText);
 
-function translateText(text) {
+// Function translate text to morse code
+const translateText = (text) => {
   // create an empty string to store the translated morse code
   let translatedMorseCode = "";
   // loop through the text
   for (let i = 0; i < text.length; i++) {
     // if the text is a space
-    if (text[i] === " ") {
+    if (text[i] === "") {
       // add a space to the translated morse code
       translatedMorseCode += " ";
     } else {
@@ -39,6 +37,32 @@ function translateText(text) {
     }
   }
   return translatedMorseCode;
+}
+
+// Function translate morse code to text
+const translateMorseCode = (morseCode) => {
+  // split the morse code into an array
+  const morseCodeArray = morseCode.split("   ");
+  // create an empty string to store the translated text
+  console.log("This is the morse code array: ", morseCodeArray)
+  let translatedText = "";
+  // loop through the morse code array
+  for (let i = 0; i < morseCodeArray.length; i++) {
+    // if the morse code is a space
+    if (morseCodeArray[i] === "") {
+      // add a space to the translated text
+      translatedText += " ";
+    } else {
+    // Use the MorseCodeAlphabetNumber object to find the corresponding letter
+    let translatedText = morseCodeArray.map(function (word) {
+      word.split(" ").map(function (letter) {
+        translatedText.push(morseCodeAlphabetNumber[letter])
+      })
+      translatedText.push(" ").join("")
+    })
+    return translatedText;
+    }
+  }
 }
 
 // Event listener for the reset button
@@ -52,4 +76,5 @@ resetButton.addEventListener("click", () => {
 // Call the function to run the program
 inputMorseCodeOrText();
 
-});
+})
+
